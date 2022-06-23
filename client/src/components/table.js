2c5e368f0cdcf24ea2164aa38ca363a1208b51af
Jacob Parris <typefortyproductions@gmail.com>
2022-06-23 19:17:00 -0400

    login and signup logic added on front end, including mutations and quieres. Now to set up back end

---
+++ client/src/components/table.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class ImageLoop extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { listOfImages: [] };
+    }
+    importAll(r) {
+      return r.keys().map(r);
+    }
+  
+    componentWillMount() {
+      const list = this.importAll(
+        require.context("../media/Portfolio/Portraits", false, /\.(jpg)$/)
+      );
+      this.setState({
+        listOfImages: list
+      });
+    }
+  
+    render() {
+      return (
+
+        
+
+        <>
+          <ul>
+            <li className="contentbox menu">
+              {this.state.listOfImages.map((image, index) => (
+                <img classname="menuli" src={image} key={index} alt="info"></img>
+              ))}
+            </li>
+          </ul>
+        </>
+      );
+    }
+  }
+  
+  export default ImageLoop;
